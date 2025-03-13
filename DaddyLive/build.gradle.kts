bbed3132ec565f6148c820abd5447b8e5736e849
federic0419 <135590399+federic0419@users.noreply.github.com>
2025-03-13 14:32:44 +0100

    added files

---
+++ DaddyLive/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 6
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Live TV from DaddyLive. You might need a VPN"
+    authors = listOf("doGior")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+
+    tvTypes = listOf("Live")
+
+    requiresResources = false
+
+    iconUrl = "https://raw.githubusercontent.com/doGior/doGiorsHadEnough/refs/heads/master/DaddyLive/live-streaming.png"
+}
